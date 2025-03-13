54262415fbdb7f00f5d47aa0c43b1e3e7f057573
Lim Zi Yang <ziyang.lim@hiredly.com>
2025-03-13 14:34:54 +0800

    experimental: test

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-'use strict';
+const compression = require('compression')
+const express = require('express')
+const Gun = require('gun')
 
-const Gun = require('gun');
-const http = require('http');
+const app = express()
+const port = process.env.PORT || 8765
 
-const port = process.env.PORT || 8080; 
-const server = http.createServer();
+app.use(compression())
+app.use(Gun.serve)
+app.use(express.static(__dirname));
 
-const gun = Gun({
-    web: server, 
-});
+const server = app.listen(port, () => {
+    console.log(`GunDB Relay Peer Listening on: http://localhost:${port}`)
+})
 
-server.listen(port, () => {
-    console.log(`GunDB server running on port ${port}`);
-});
+app.get('/', (req, res) => {
+    res.send(`GunDB Relay Peer Running on port: ${port}`)
+})
+
+Gun({ web: server })
